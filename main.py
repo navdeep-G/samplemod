9ec601dc97b0fc724c42746483368904b055de41
S-T-A-L-K-R <name.generate@gmail.com>
2022-05-17 15:22:04 +0200

    added main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import sample.core
+import sample.helpers
+
+print(sample.core.get_hmm())
+
+if sample.helpers.get_answer() == True:
+    print("It works!!!")
+else:
+    print("It doesn't work!!!")
