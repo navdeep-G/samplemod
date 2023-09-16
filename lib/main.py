8a90687ac0da7517edd273c7867793b1edb445d2
dannydaniil <danny.daniil@uniswap.org>
2023-09-16 19:44:18 -0400

    pulls data

--- lib/main.py
+++ lib/main.py
@@ -1,5 +1,11 @@
 import yaml
 import os
+from google.cloud import bigquery
+from google.oauth2 import service_account
+import json
+
+
+BIG_QUERY_PROJECT_ID = 'tranquil-tiger-399222'
 
 def read_yaml_file(filename):
     current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -8,6 +14,25 @@ def read_yaml_file(filename):
         data = yaml.safe_load(file)
     return data
 
+def read_json_file(filename):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(current_dir, filename)
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return data
+
 if __name__ == "__main__":
-    content = read_yaml_file('./config.yml')
-    print(content)
+    configFile = read_yaml_file('./config.yml')
+    bigQueryCredentialsFile = read_json_file('./keys.json')
+
+    credentials = service_account.Credentials.from_service_account_info(
+        bigQueryCredentialsFile
+    )
+    bigQueryClient = bigquery.Client(
+        project=BIG_QUERY_PROJECT_ID,
+        credentials=credentials
+    )
+    result = bigQueryClient.query(
+        query='SELECT * FROM `bigquery-public-data.austin_311.311_service_requests` LIMIT 10'
+    ).to_dataframe()
+    print(result)
