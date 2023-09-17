6b492633a21cd3984bdeb17393f39b715f8b52dc
dannydaniil <danny.daniil@uniswap.org>
2023-09-17 19:57:28 -0400

    transaltes yaml to schema for 1 table

--- lib/main.py
+++ lib/main.py
@@ -5,6 +5,7 @@ from google.oauth2 import service_account
 import json
 
 
+
 def read_yaml_file(filename):
     current_dir = os.path.dirname(os.path.abspath(__file__))
     file_path = os.path.join(current_dir, filename)
@@ -19,18 +20,43 @@ def read_json_file(filename):
         data = json.load(file)
     return data
 
+def embeddingsConfigToQuery(embeddingsConfig):
+    embeddingsQueries = []
+    for embeddingIndex, embeddingConfig in enumerate(embeddingsConfig):
+        for columnIndex, column in enumerate(embeddingConfig['columns']):
+            if len(embeddingsQueries) < embeddingIndex + 1:
+                embeddingsQueries.append('SELECT ')
+            embeddingsQueries[embeddingIndex] += column['name']
+            if columnIndex < len(embeddingConfig['columns']) - 1:
+                embeddingsQueries[embeddingIndex] += ' , '
+
+        embeddingsQueries[embeddingIndex] += ' FROM ' + embeddingConfig['table']
+        embeddingsQueries[embeddingIndex] += ' LIMIT 10'
+    return embeddingsQueries
+
+def getScehmaFromTable(table):
+    return bigqueryClient.get_table(
+        table
+    ).schema
+
+
 if __name__ == "__main__":
     configFile = read_yaml_file('./config.yml')
     bigQueryCredentialsFile = read_json_file('./keys.json')
 
-    credentials = service_account.Credentials.from_service_account_info(
+    embeddingsQuery = embeddingsConfigToQuery(configFile['embeddings'])
+    print(embeddingsQuery)
+
+    bigqueryCredentials = service_account.Credentials.from_service_account_info(
         bigQueryCredentialsFile
     )
-    bigQueryClient = bigquery.Client(
+    bigqueryClient = bigquery.Client(
         project=bigQueryCredentialsFile['project_id'],
-        credentials=credentials
+        credentials=bigqueryCredentials
     )
-    result = bigQueryClient.query(
-        query='SELECT * FROM `bigquery-public-data.austin_311.311_service_requests` LIMIT 10'
+
+
+    result = bigqueryClient.query(
+        query=embeddingsQuery[0]
     ).to_dataframe()
     print(result)
