eedf17437b112d514b0bf677dad08b0adf36c9ea
Giorgos Vasilakis <vasilakisg2@gmail.com>
2023-11-10 13:01:21 -0500

    add jupyter notebooks and predict functions

---
+++ scripts/main.py
@@ -0,0 +1,13 @@
+from run_predict import predict
+from run_update import update
+from run_evaluate import evaluate
+
+
+def main():
+    predict()
+    update()
+    evaluate()
+
+
+if __name__ == "__main__":
+    main()
